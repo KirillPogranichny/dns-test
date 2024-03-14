4a21bb38be7b696723c55c9f1258f8000caa65dc
KirillPogranichny <kirillpogranichny@gmail.com>
2024-03-14 20:44:14 +1000

    add support for creating PK and FK for tables

--- load_to_db.py
+++ load_to_db.py
@@ -47,11 +47,11 @@ def execute_values(conn, df, table):
         conn.rollback()
         cursor.close()
         return 1
-    print(f'Data has been inserted into {table}')
+    print(f'Data has been inserted into {table} table\n')
     cursor.close()
 
 
-def create_table_from_csv(conn, csv_file, table_name):
+def create_table_from_csv(conn, csv_file, table_name, primary_key_column=None, foreign_key_columns=None):
     # Load data into the table
     data = pd.read_csv(csv_file)
     data = data.drop('Unnamed: 0', axis=1)
@@ -68,8 +68,19 @@ def create_table_from_csv(conn, csv_file, table_name):
 
     # If table does not exist, create it
     if not table_exists:
-        columns = ','.join([f'{col} {dtype_mapping(data[col])}' for col in data.columns])
-        create_table_sql = f"CREATE TABLE {table_name} ({columns})"
+        columns = [f'{col} {dtype_mapping(data[col])}' for col in data.columns]
+
+        # Creating columns with PK and FK according to data from the dictionary
+        if primary_key_column:
+            columns.append(f'PRIMARY KEY ({primary_key_column})')
+        if foreign_key_columns:
+            for column, refs_list in foreign_key_columns.items():
+                for refs in refs_list:
+                    ref_table, ref_column = refs[0], refs[1]
+                    columns.append(f'FOREIGN KEY ({column}) REFERENCES {ref_table}({ref_column})')
+
+        columns_str = ','.join(columns)
+        create_table_sql = f"CREATE TABLE {table_name} ({columns_str})"
         cursor = conn.cursor()
         cursor.execute(create_table_sql)
         conn.commit()
@@ -85,15 +96,22 @@ if __name__ == "__main__":
 
     # Table creation and data insertion for each CSV file
     csv_files_info = [
-        {'file': 'data/t_branches.csv', 'table_name': 'branches'},
-        {'file': 'data/t_cities.csv', 'table_name': 'cities'},
-        {'file': 'data/t_products.csv', 'table_name': 'products'},
-        {'file': 'data/t_sales.csv', 'table_name': 'sales'}
+        {'file': 'data/t_cities.csv', 'table_name': 'cities', 'primary_key_column': 'Ссылка'},
+        {'file': 'data/t_branches.csv', 'table_name': 'branches',
+         'primary_key_column': 'Ссылка',
+         'foreign_key_columns': {'Город': [('cities', 'Ссылка')]}},
+        {'file': 'data/t_products.csv', 'table_name': 'products',
+         'primary_key_column': 'Ссылка'},
+        {'file': 'data/t_sales.csv', 'table_name': 'sales',
+         'foreign_key_columns': {'Филиал': [('branches', 'Ссылка')],
+                                 'Номенклатура': [('products', 'Ссылка')]}}
     ]
 
     for csv_file_info in csv_files_info:
         csv_file_path = csv_file_info['file']
         table_name = csv_file_info['table_name']
+        primary_key_column = csv_file_info.get('primary_key_column')
+        foreign_key_columns = csv_file_info.get('foreign_key_columns')
 
         # Check if file exists
         if not pd.io.common.file_exists(csv_file_path):
@@ -101,7 +119,7 @@ if __name__ == "__main__":
             continue
 
         print(f"Creating table {table_name} from {csv_file_path}")
-        create_table_from_csv(conn, csv_file_path, table_name)
+        create_table_from_csv(conn, csv_file_path, table_name, primary_key_column, foreign_key_columns)
 
     # Close the connection
     conn.close()
